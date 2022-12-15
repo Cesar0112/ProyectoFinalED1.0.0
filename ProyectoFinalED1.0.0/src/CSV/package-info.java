318545076a284d02b7bec7a0e1ffd4a2e9c4ee15
Cesar <98931642+Cesar0112@users.noreply.github.com>
2022-12-14 21:54:25 -0500

    Delete ProyectoFinalED1.0.0 directory
    Borre la version 1.0.0 para agregar la nueva

--- ProyectoFinalED1.0.0/src/CSV/package-info.java
+++
@@ -1,6 +0,0 @@
-package CSV;
-/**
- * Este paquete esta hecho con la intencion de
- * leer los archivos csv que funcionan como base de datos
- * y almacenarlos en el*/
- 
--- ProyectoFinalED1.0.0/src/principal/package-info.java
+++
@@ -1,4 +0,0 @@
-package principal;
-/**
- * este paquete contiene la clase principal donde va a correr la aplicacion
- */
